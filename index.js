0ffdbdc6e164a92b0639a549fdadec329f5d208b
Lisa Alusa <sayilisaalusa@gmail.com>
2025-03-09 19:24:57 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+// Function 1: distanceFromHqInBlocks
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42; 
+    return Math.abs(pickupLocation - hqLocation); 
+}
+
+// Function 2: distanceFromHqInFeet
+function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    return blocks * 264; 
+}
+
+// Function 3: distanceTravelledInFeet
+function distanceTravelledInFeet(startBlock, endBlock) {
+    return Math.abs(startBlock - endBlock) * 264; 
+}
+
+// Function 4: calculatesFarePrice
+function calculatesFarePrice(startBlock, endBlock) {
+    const distance = distanceTravelledInFeet(startBlock, endBlock); 
+
+    if (distance <= 400) {
+        return 0; 
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02; 
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far'; 
+    }
+}
+
